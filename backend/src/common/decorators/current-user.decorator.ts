d58bf9e9d7251f1fa0d473974aeb7284e6c157bc
devex <loan.mata@ynov.com>
2026-06-22 01:25:17 +0200

    feat: add optional-auth guard and current-user decorator

--- backend/src/common/decorators/current-user.decorator.ts
+++ backend/src/common/decorators/current-user.decorator.ts
@@ -4,6 +4,16 @@ export const CurrentUser = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const user = request.user;
+
+    return data ? user?.[data] : user;
+  },
+);
+
+export const CurrentUserOptionnal = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
+    if(!user) return null
     return data ? user?.[data] : user;
   },
 );
